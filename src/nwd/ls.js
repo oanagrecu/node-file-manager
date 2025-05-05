b242660a987dba71f1057d7a2c31bad206ca5337
oanagrecu <oana.alexandra.grecu@gmail.com>
2025-05-05 22:17:41 +0200

    fixing ls.js issues

--- src/nwd/ls.js
+++ src/nwd/ls.js
@@ -3,19 +3,22 @@ import {promises as fs} from "node:fs";
 export const ls = async (currentDir) => {
 	try {
 		const entries = await fs.readdir(currentDir, {withFileTypes: true});
-		const dirs = [],
-			files = [];
 
-		for (const entry of entries) {
-			if (entry.isDirectory()) dirs.push(entry.name);
-			else files.push(entry.name);
-		}
+		const formattedEntries = entries
+			.map((entry) => ({
+				Name: entry.name,
+				Type: entry.isDirectory() ? "DIR" : "file",
+			}))
+			.sort((a, b) => {
+				if (a.Type !== b.Type) {
+					return a.Type === "DIR" ? -1 : 1;
+				}
+				return a.Name.localeCompare(b.Name);
+			});
 
-		dirs.sort();
-		files.sort();
-
-		dirs.forEach((dir) => console.log(`DIR   ${dir}`));
-		files.forEach((file) => console.log(`file  ${file}`));
+		formattedEntries.forEach((entry, index) => {
+			console.log(`${index}\t'${entry.Name}'\t'${entry.Type}'`);
+		});
 	} catch (err) {
 		console.log("Operation failed");
 	}
